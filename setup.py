ca358421d1f3b312e6a7b1b6520185c5ee93f107
risan <risanbagja@gmail.com>
2016-03-13 15:18:28 +0100

    Update patch version.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
 
 setup(
     name='irisflower',
-    version='1.0.1',
+    version='1.0.2',
     description='Iris flower species classifier',
     long_description=long_description,
     url='https://github.com/risan/iris-flower-classifier',
