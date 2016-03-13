73aa56610319296032a55b2d63e6d7a03a34c1f2
risan <risanbagja@gmail.com>
2016-03-13 13:43:43 +0100

    Update requirements.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     keywords='machine learning classifier',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     install_requires=[
-        'scipy>=0.9',
+        'scipy',
         'scikit-learn'
     ],
 )
